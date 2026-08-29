cbff16a194da6dfac47a79adf1f638eb4a580314
CanooWeb <safried967@gmail.com>
2026-08-29 05:22:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -49,6 +49,16 @@ function verifyPassword(password, stored) {
 app.use(express.json());
 app.use(express.static(__dirname));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.post('/api/register', (req, res) => {
   const { username, password } = req.body || {};
 
